be322c87bd6b6010e82a422c8a9593678edd7e0c
Ziyauvesh <ziyauvesh4321@gmail.com>
2023-09-08 10:12:57 +0530

    My Ducat project

---
+++ src/About.js
@@ -0,0 +1,17 @@
+function Uvi()
+{
+    return <p>Hi i am Muhd Uves</p>;
+}
+
+export default Uvi 
+
+export function Uv()
+{
+    return "this is defined export function";
+}
+
+export function Clr()
+{
+    return <h1>this is h1 function</h1>
+}
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Uvi from './About';
+import { Uv } from './About';
+import { Clr } from './About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <Uvi></Uvi>
+    <Clr></Clr>
+    <Uv></Uv>
   </React.StrictMode>
 );
 
